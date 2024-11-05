0e24d9d85e2ddb6661d269431ea70ca2c8a4ece6
pwestland <pwestland@smith.edu>
2024-11-05 18:28:21 -0500

    engine test draft

--- Engine.java
+++ Engine.java
@@ -44,3 +44,11 @@ public class Engine {
     }
 
 }
+
+public static void main(String[] args) {
+    Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
+    while (myEngine.go()) {
+        System.out.println("Choo choo!");
+    }
+    System.out.println("Out of fuel.");
+}

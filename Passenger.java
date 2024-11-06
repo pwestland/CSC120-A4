b6ff854db4c8909e3225d21211938ebc2768ba15
pwestland <pwestland@smith.edu>
2024-11-05 19:30:07 -0500

    passenger done

--- Passenger.java
+++ Passenger.java
@@ -5,4 +5,26 @@ public class Passenger {
     public Passenger(String name) {
         this.name = name;
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public void boardCar(Car c){
+        if (c.addPassenger(this) == true){
+            System.out.println("Passenger has boarded this car");
+        }
+        else{
+            System.out.println("Passenger cannot board this car");
+        }
+    }
+
+    public void getOffCar(Car c){
+        if (c.removePassenger(this) == true){
+            System.out.println("Passenger has left this car");
+        }
+        else{
+            System.out.println("Passenger cannot leave this car");
+        }
+    }
 }
